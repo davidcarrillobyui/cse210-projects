7c194ebcfffd0712c913a1fa036c33acf65eb645
David Carrillo <vrunitybro@gmail.com>
2024-05-01 23:22:01 -0600

    Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0. Compute the sum, or total, of the numbers in the list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
+
+/*
+1.Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+2.Compute the sum, or total, of the numbers in the list.
+*/
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        while(true)
+        {
+            Console.Write("Enter a number (Enter 0 to stop): ");
+            int input = int.Parse(Console.ReadLine());
+
+            if(input==0)
+            {
+                break;
+            }
+
+            numbers.Add(input);
+        }
+
+        int sum = 0;
+
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine("The sum of the numbers is: " + sum);
     }
 }
