5fb472c9641ac00ae6d184f881ed9dfae4354342
David Carrillo <vrunitybro@gmail.com>
2024-05-02 01:27:57 -0600

    write a C# program that has several simple functions:
    DisplayWelcome - Displays the message, "Welcome to the Program!"
    PromptUserName - Asks for and returns the user's name (as a string)
    PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
    SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
    DisplayResult - Accepts the user's name and the squared number and displays them.
    Your Main function should then call each of these functions saving the return values and passing data to them as necessary.
    
    Sample output of the program could look as follows:
    
    Welcome to the program!
    Please enter your name: Brother Burton
    Please enter your favorite number: 42
    Brother Burton, the square of your number is 1764

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -7,12 +7,44 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
         Welcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptFavoriteNumber();
+        int squareNumber = SquareNumber(favoriteNumber);
+        ShowResult(userName, squareNumber);
     }
 
     static void Welcome()
     {
         Console.WriteLine("Welcome to the Program");
     }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name?: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptFavoriteNumber()
+    {
+        Console.Write("Type your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+
+        return square;
+    }
+
+    static void ShowResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your favorite number is {square}.");
+    }
+
+
 
    
 }
