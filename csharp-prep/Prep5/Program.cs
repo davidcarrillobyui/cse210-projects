7766a6c96039f46aad51d9b0656a308e8d39829e
David Carrillo <vrunitybro@gmail.com>
2024-05-02 00:23:58 -0600

    DisplayWelcome - Displays the message, "Welcome to the Program!"

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        Welcome();
     }
+
+    static void Welcome()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+   
 }
