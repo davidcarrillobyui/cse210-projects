264b4e9ba3fab9105634083739d4d176deb807b0
davidcarrillobyui <vrunitybro@gmail.com>
2024-06-06 23:24:20 -0600

    math assigment

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,13 @@
+
+
+public class MathAssignment  : Assigment
+{
+    private string _textBookSection;
+    private string _problem;
+
+    public MathAssignment(string studentName, string topic, string textBookSection, string problem)
+    {
+        
+    }
+
+}
