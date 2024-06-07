690fe8cdf7f63f64b3f0f2cb6db38e336fd7b427
davidcarrillobyui <vrunitybro@gmail.com>
2024-06-06 23:29:21 -0600

    math class get homework done

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -5,9 +5,21 @@ public class MathAssignment  : Assigment
     private string _textBookSection;
     private string _problem;
 
-    public MathAssignment(string studentName, string topic, string textBookSection, string problem)
+    public MathAssignment(string studentName, string topic, string textBookSection, string problem) : base(studentName, topic)    
     {
-        
+        _textBookSection = textBookSection;
+        _problem = problem;
+
     }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textBookSection} Problems {_problem}";
+    }
+
+
+
+
+
 
 }
