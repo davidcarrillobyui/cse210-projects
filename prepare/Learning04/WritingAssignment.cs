f4df031d352f4615aee658bcab8485228b92a2cd
davidcarrillobyui <vrunitybro@gmail.com>
2024-06-06 23:32:30 -0600

    writting assigment

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,12 @@
+public class WritingAssignment : Assigment
+
+{
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string _title) : base (studentName, topic)
+    {
+        _title = title;
+
+    }
+
+
+}
