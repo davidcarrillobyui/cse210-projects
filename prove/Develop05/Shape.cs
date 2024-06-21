5415cad0de4f6678ac9dc7d0f1be19a5a089dc22
davidcarrillobyui <vrunitybro@gmail.com>
2024-06-20 23:31:15 -0600

    shape square and rectable classes created

--- prove/Develop05/Shape.cs
+++ prove/Develop05/Shape.cs
@@ -1,5 +1,18 @@
 public abstract class Shape
-{ 
+{
+    private string _color;
+
+    public Shape (string color)
+    {
+        _color=color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public abstract double GetArea();
 
 
 
